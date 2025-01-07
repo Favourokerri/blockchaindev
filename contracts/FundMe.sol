5735bb53d3682374cb7bcf2326fc90635c017d0a
Favourokerri <favourokerri767@gmail.com>
2025-01-07 19:12:37 +0100

    constructor

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -14,11 +14,41 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addresSTOAmountFunding; 
 
+    address public Owner;
+
+    //constructors run once the contract is deployed
+    // the constuctor below will set the owner of thecontract
+    // to the address used in deploying it
+    constructor() {
+        Owner = msg.sender;
+    } 
+
     function Fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "not enough eth");
         funders.push(msg.sender);
         addresSTOAmountFunding[msg.sender] += msg.value;
     }
+
+    function withdraw() public{
+        //make sure only owner can call this function
+        require(msg.sender == Owner, "you dont have the permission to call this function");
+        for(uint256 i=0; i<funders.length; i++) {
+            address funder = funders[i];
+            addresSTOAmountFunding[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        //this is the safest way to withdraw money in solidity since it also 
+
+        //reverts if an errror should occour
+        // payable(msg.sender).transfer(address(this).balance);
+
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "call failed");
+
+    }
+
 
     function getVersion() public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
